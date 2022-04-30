6f543532f8bff99ca6de2ad2a896e384a5744a10
Rodrigo Romero <rodrigoromero2308@gmail.com>
2022-04-30 16:08:03 -0300

    more code splitting

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import { StrictMode } from "react";
+import { lazy, StrictMode, Suspense } from "react";
 import { render } from "react-dom";
 import "./variables.css";
 import "normalize.css";
 import "./main.css";
-import MainComponent from "./mainComponent";
+import { LoadingOverlay } from "@mantine/core";
+
+const MainComponent = lazy(() => import("./mainComponent"));
 
 render(
   <StrictMode>
-    <MainComponent />
+    <Suspense fallback={<LoadingOverlay visible />}>
+      <MainComponent />
+    </Suspense>
   </StrictMode>,
   document.getElementById("root")
 );
